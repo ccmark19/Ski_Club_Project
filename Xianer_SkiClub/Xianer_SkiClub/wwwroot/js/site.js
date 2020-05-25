b0be25f0c2341241eae314c89f9b95c675f7456e
ccmark19 <cc.mark19@gmail.com>
2020-05-25 07:39:20 -0400

    added calendar feature and js file

--- Xianer_SkiClub/Xianer_SkiClub/wwwroot/js/site.js
+++ Xianer_SkiClub/Xianer_SkiClub/wwwroot/js/site.js
@@ -1,4 +1,16 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+
+document.addEventListener('DOMContentLoaded', function () {
+    var calendarEl = document.getElementById('calendar');
+
+    var calendar = new Calendar(calendarEl, {
+        plugins: [dayGridPlugin]
+    });
+
+    calendar.render();
+});
